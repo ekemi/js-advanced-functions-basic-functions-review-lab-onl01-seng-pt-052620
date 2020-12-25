d2e900e6ca1b47cb59224ca383241e2c5dd7ff04
edmundo papi <edmundopierre@yahoo.com>
2020-12-25 17:03:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,5 +14,7 @@ return function(str = 'special'){
 
 const Calculator = {
   add: function(a,b){return a+b},
-
+  subtract: function(a,b){return a-b},
+  multiply: function(a,b){return a*b},
+  divide: function(a,b){return a/b}
 };
